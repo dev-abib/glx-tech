46c0652fd23af64198366dd485658dcc5d5bd8f8
sultan93291 <abibdipto@gmail.com>
2026-07-15 15:11:42 +0600

    feat: updated user && home hero model

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -74,6 +74,8 @@ router.route("/switch-role").post(authenticate(), validate(switchRoleSchema), sw
 router.route("/gt-all-users").get(authenticate({ type: "admin" }), getAllUsers);
 
 // update user to seller
-router.route("/update-as-seller").post(authenticate({type:'user'}), validate(updateUserAsSellerSchema), updateAsSeller)
+router.route("/update-as-seller").post(authenticate({ type: 'user' }), validate(updateUserAsSellerSchema), updateAsSeller)
+
+router.route("")
 
 export default router;
